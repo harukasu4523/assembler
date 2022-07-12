1bfd2f0f8007e585a37e585bd7689059f17fb561
harukasu4523 <airgir23@gmail.com>
2022-07-12 23:17:52 +0900

    aaaa

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,9 @@ fn main() {
     let contents = read_file(&mut f);
     // TODO: 中身を一行ずつ取り出してA命令かC命令かを判断する
     for line in contents.lines() {
+        // TODO: 空行をなくした文字列のみ取り出す
+        // TODO: コメントアウトの行を飛ばす
+        // if line.char
         if is_a_order(&line) {
             println!("{line} is A order!");
             // TODO: A命令のときの処理
@@ -24,7 +27,15 @@ fn main() {
 }
 
 fn is_a_order(line: &str) -> bool {
-    line.chars().nth(0).unwrap() == '@'
+    //　空行を飛ばす処理を別関数に移すと思われる
+    let mut first_string: usize = 0;
+    let cs: String = line.to_string();
+    for c in cs.as_str().chars(){
+        if c == ' ' {
+            first_string += 1;
+        }
+    }
+    line.chars().nth(first_string).unwrap() == '@'
 }
 
 fn read_file(f: &mut File) -> String {
