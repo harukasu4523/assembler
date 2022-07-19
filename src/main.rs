b83161b5cfb4eeff583a8d8c2119248b4478694f
harukasu4523 <airgir23@gmail.com>
2022-07-19 23:21:03 +0900

    refactoring read file function

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::Error;
 
-struct Command_type{
-    a_command: bool,
-    c_command: bool,
-    l_command: bool,
+struct CommandType{
+    a: bool,
+    c: bool,
+    l: bool,
     comment: bool,
 }
+
 
 fn main() {
     // 初期化
     let args: Vec<String> = env::args().collect();
-    let mut order =  Command_type{
-        a_command:false,
-        c_command:false,
-        l_command:false,
+    let mut command =  CommandType{
+        a:false,
+        c:false,
+        l:false,
         comment:false
     };
     //　引数合わせる
@@ -23,27 +25,48 @@ fn main() {
         panic!("Not enough arguments");
     }
     // ファイルの読み込み
-    let mut f = File::open(&args[1]).expect("Not open this file");
-    let contents = read_file(&mut f);
+    let mut contents = String::new();
+    match get_file_contents(&args[1]) {
+        Ok(contents_rerurn) =>{
+            println!("Ok: file_open");
+            contents = contents_rerurn;
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+        }
+    }
+
     // 中身を一行ずつ取り出してA命令かC命令かを判断する
     for line in contents.lines() {
+        // println!("{}",line.len());
+        if line.len() == 0{
+            continue;
+        }
         // 先にコメントアウトがあるかを調べて、あればコメントより前にコマンド部分があるか調べる
-        order.comment = is_comment(&line);
+        command.comment = is_comment(&line);
+        // TODO: 改行のみの箇所を飛ばすようにする
+    
         //  空行とコメントをなくした文字列のみ取り出す
-        let syntax: String = pick_str(&line, &order);
-        is_order(&syntax, &order); 
+        let syntax: String = pick_str(&line, &command);
+        is_command(&syntax, &mut command); 
         //  コメントアウトのみの行を飛ばす
-        if order.comment {
+        if command.comment {
             println!("found comment!!!!");
             continue;
         }
 
-        if order.a_command {
-            println!("{syntax} is A order!");
+        if command.a {
+            println!("{syntax} is A command!");
+            command.a = false;
             // TODO: A命令のときの処理
-        } else {
-            println!("{syntax} is C order!");
+        } else if command.c {
+            println!("{syntax} is C command!");
+            command.c = false;
+
             // TODO: C命令のときの処理
+        }else{
+            command.l = false;
+            println!("{syntax} is synbol");
         }
     }
 
@@ -64,7 +87,7 @@ fn is_comment(line: &str) -> bool {
     false
 }
 
-fn pick_str(line: &str, order: &Command_type) -> String {
+fn pick_str(line: &str, command: &CommandType) -> String {
     // 　空行を含まない文字列のみを抽出する。
     let v: Vec<&str> = line.split_whitespace().collect();
     let mut result = String::new();
@@ -74,24 +97,31 @@ fn pick_str(line: &str, order: &Command_type) -> String {
         result.push_str(i);
     }
     // コメントがある時
-    if order.comment {
+    if command.comment {
         let s: Vec<&str> = result.matches("//").collect();
         return s[0].to_string();
     }
     result
 }
 
-fn is_order(line: &str, order:  &mut Command_type) {
+fn is_command(line: &str, command:  &mut CommandType) {
     // A命令C命令その他を判断するC:dest=comp;jmp '=' or ';'が含まれているかどうかで判断
-    if line.chars().nth(0).unwrap() == '@'{
-        order.a_command = true;
-    }
+        if line.chars().nth(0).unwrap() == '@'{
+            command.a = true;
+            return();
+        }
+        for c in line.chars() {
+            if c == '=' || c == ';' {
+                command.c = true;
+                return();
+            }
+        }
+        command.l = true;
 }
 
-fn read_file(f: &mut File) -> String {
-    let mut content = String::new();
-    f.read_to_string(&mut content)
-        .expect("failed to read the file");
-
-    content
+fn get_file_contents(file_path: &String) -> Result<String, Error>{
+    let mut f = File::open(file_path.trim())?;
+    let mut return_contents = String::new();
+    f.read_to_string(&mut return_contents)?;
+    Ok(return_contents)
 }
